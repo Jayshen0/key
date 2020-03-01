10350d3f65f0f6f4e81d0386cdfbb4d23619b90e
Jayshen0 <55937530+Jayshen0@users.noreply.github.com>
2020-02-29 17:59:41 -0800

    Update d_train.py

--- d_train.py
+++ d_train.py
@@ -15,28 +15,6 @@ df.iloc[:,8]=df.iloc[:,8].fillna(method='ffill')
 
 df = df[df['Start Date'] > '2010']
 
-d = dict()
-
-d['British Columbia'] = [1 for i in range(11)]
-
-for region in ['Alberta','Manitoba','Ontario','Québec','Saskatchewan']:
-    cur = df[df['Region']==region]
-    now = [0 for i in range(11)]
-    for i in range(2016,2020):
-        tmp = cur[cur['Start Date'] < str(i)]
-        cur = cur[cur['Start Date'] > str(i)]
-        tmp = list(tmp['Value'].values)
-        
-
-        for j in range(11):
-            now[j] += tmp[j+1] / tmp[j]
-    
-    for k in range(11):
-        now[k] /= 4
-    
-    d[region] = now
-            
-
 
 
 
@@ -45,17 +23,18 @@ sub = pd.read_csv('./solution.csv')
 i = 0
 for r in ['Alberta','British Columbia', 'Manitoba', 'Ontario','Québec','Saskatchewan']:
     cur = df[df['Region']==r]
-    
-    sub.iloc[i*12,-2] = (float(cur[cur['Start Date']=='2018/1/1']['Value'])+float(cur[cur['Start Date']=='2017/1/1']['Value']))/2   
-    for j in range(1,12):
-        sub.iloc[i*12+j,-2] = sub.iloc[i*12+j-1,-2] * d[r][j-1]
+
+    for j in range(12):
+        sub.iloc[i*12+j,-2] = (float(cur[cur['Start Date']=='2018/'+str(j+1)+'/1']['Value'])+\
+                float(cur[cur['Start Date']=='2017/'+str(j+1)+'/1']['Value']))/2   
+        
+        if sub.iloc[i*12+j,-2] == 0:
+            sub.iloc[i*12+j,-2] = 700
     
     i += 1
     
 
 
-        
-        
         
 
 
