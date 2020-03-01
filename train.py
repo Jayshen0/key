af4973d370f4752358019db5e06f91c2c0f8d525
Jayshen0 <55937530+Jayshen0@users.noreply.github.com>
2020-03-01 00:14:29 -0800

    Update train.py

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ for i in range(3,df.shape[0]):
     
     if df.iloc[i,6] > '2018':
         pre_x[df.iloc[i,2]].append(data_scaled[i-3:i,0])
-        pre_y.append(0)
+        pre_y.append(data_scaled[i,0])
 
 
 train = myDataset(train_x,train_y)
