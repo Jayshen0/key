09bb465e0625eb8c56eed1b6ee58cecb78056175
AhJay <1768063893@qq.com>
2020-03-01 01:01:34 -0800

    first

--- d_train.py
+++ d_train.py
@@ -17,32 +17,22 @@ import torch.nn as nn
 import torch.optim as optim
 import torch
 
-m = 'Saskatchewan'
+
 df = pd.read_csv('./Challenge_Data/1. Target Variables/Canada_Canola_Producer_Deliveries.csv')
-
 df.iloc[:,8]=df.iloc[:,8].fillna(method='ffill')
+df=df[df['Region'] == 'Alberta']
 
 
-
-for i in range(df.shape[0]):
-    if df.iloc[i,2] == 'Alberta' and df['Start Date'].iloc[i] >'2006':
-        df.ioc[i,-2] = '0'
-    
-    if df.iloc[i,2] == 'Manitoba' and df['Start Date'].iloc[i] > '2007':
-        df.ioc[i,-2] = '0'
-        
-    
-    if df.iloc[i,2] == m and df['Start Date'].iloc[i] > '2005':
-        df.ioc[i,-2] = '0'        
-        
+data = df.iloc[:,8].values
+data = data.reshape([-1,1])
 
 min_v = min(data)
 max_v = max(data)
 range_v = max_v - min_v
 
-
-for i in range(len(data)):
-    data[i] = (data[i]-min_v) / range_v
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+data_scaled = sc.fit_transform(data)
 
 #harv = pd.read_csv('./Challenge_Data/2. Other Canola Production Data/Canada_Canola_Harvested_Area.csv')
 #harv = harv[harv['Region'] == 'Alberta']
@@ -50,22 +40,30 @@ for i in range(len(data)):
 train_x = []
 train_y = []
 
+test_x = []
+test_y = []
+
 pre_x = defaultdict(list)
 pre_y = []
 
-for i in range(len(data)):
+for i in range(3,df.shape[0]):
     if df.iloc[i-3,2] != df.iloc[i,2]:
         continue
     
-    train_x.append(data[i-3:i])
-    train_y.append(data[i])
+    if df.iloc[i,6] < '2018':
+        train_x.append(data_scaled[i-3:i,0])
+        train_y.append(data_scaled[i,0])
+    else:
+        test_x.append(data_scaled[i-3:i,0])
+        test_y.append(data_scaled[i,0])
     
     if df.iloc[i,6] > '2018':
-        pre_x[df.iloc[i,2]].append(data[i-3:i])
-        pre_y.append(0)
+        pre_x[df.iloc[i,2]].append(data_scaled[i-3:i,0])
+        pre_y.append(data_scaled[i,0])
 
 
 train = myDataset(train_x,train_y)
+test = myDataset(test_x,test_y)
 pre_d = dict()
 for r in pre_x:
     pre_d[r] =DataLoader(myDataset(pre_x[r],pre_y), batch_size=1,num_workers=4,shuffle=False) 
@@ -78,17 +76,18 @@ model = model.to(device)
 
 criterion = nn.MSELoss().to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.01)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 3, gamma=0.7, last_epoch=-1)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 4, gamma=0.7, last_epoch=-1)
 
 train_loader = DataLoader(train, batch_size=1,num_workers=4,shuffle=False)
 
 epoch = 0
 prev = float('inf')
-tot_loss = 0
 
+model.load_state_dict(torch.load('best_model.pkl'))
 
-while(True):
+while(False):
     epoch += 1
+    tot_loss = 0
     rel_err = 0
     for idx, data in enumerate(train_loader):
         optimizer.zero_grad()
@@ -100,8 +99,6 @@ while(True):
         score = model.forward(x)
         score = score.view([1])
         loss = criterion(score, label)
-        
-        tot_loss += float(loss)
         loss.backward()
         optimizer.step()
         
@@ -113,14 +110,73 @@ while(True):
     print(epoch,rel_err/len(train_loader))
     
     
+    for idx, data in enumerate(train_loader):
+        optimizer.zero_grad()
+        x, label = data
+        x = x.float()
+        x = x.to(device)
+ 
+        label = label.to(device)
+        score = model.forward(x)
+        score = score.view([1])
+        loss = criterion(score, label)
+        tot_loss += float(loss)
+    
+    print(tot_loss)
     if tot_loss > prev:
         break
     
+    if epoch == 30:
+        break
+        
+    
     prev = tot_loss
-   
+    
+#torch.save(model.state_dict(), 'best_model.pkl')
 
 
-torch.save(model.state_dict(), 'best_model_d.pkl')
+sub = pd.read_csv('./Challenge_Data/to_be_filled.csv')
+
+
+for i in range(sub.shape[0]):
+    if sub.iloc[i,1][6] == '1':
+        
+        for idx, data in enumerate(pre_d[sub.iloc[i,0]]):
+            optimizer.zero_grad()
+            x,label = data
+            label = label.float()
+            label = label.to(device)
+            x = x.float()
+            x = x.to(device)
+            x = x.view([1,3])
+            score = model.forward(x)
+            loss = criterion(score, label)
+            loss.backward()
+            optimizer.step()
+       
+        
+        prev = []
+        tmp = df[df['Region']==sub.iloc[i,0]]
+        prev.append(float(tmp[tmp['Start Date']=='2018-10-01']['Value']))
+        prev.append(float(tmp[tmp['Start Date']=='2018-11-01']['Value']))
+        prev.append(float(tmp[tmp['Start Date']=='2018-12-01']['Value']))
+        prev[0] = (prev[0]-min_v)/range_v
+        prev[1] = (prev[1]-min_v)/range_v
+        prev[2] = (prev[2]-min_v)/range_v
+        
+ 
+        
+    else:
+        prev = prev[1:] + [np.array([last])]
+    
+    print(prev)
+    last = model.forward(torch.Tensor(prev).view([1,3]).to(device))
+    last = float(last)
+    print(range_v*last + min_v)
+    
+
+
+
 
 
 
