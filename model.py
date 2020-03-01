5a0684d5bfd8ec607e3ebd6f2de38ee8244c75a0
Jayshen0 <55937530+Jayshen0@users.noreply.github.com>
2020-03-01 01:23:48 -0800

    Update model.py

--- model.py
+++ model.py
@@ -9,12 +9,11 @@ class lstm(nn.Module):
     def __init__(self, feature_size, hidden_size=3, num_layers=1):
      
         super(lstm, self).__init__()
-        self.lstm = nn.LSTM(feature_size, hidden_size, num_layers, batch_first=True)
-        self.linear = nn.Linear(hidden_size, 1)
+        self.linear = nn.Linear(15, 1)
         
     def forward(self, features):
 
-        hiddens, _ = self.lstm(features.unsqueeze(1))
-        outputs = self.linear(hiddens[0])
+
+        outputs = self.linear(features)
         return outputs
 
